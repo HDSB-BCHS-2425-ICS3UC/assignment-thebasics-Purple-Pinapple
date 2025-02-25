c452b2051b5d03eeb2a54ecb6757b28e39fdecd6
Purple-Pinapple <1asparishr2@hdsb.ca>
2025-02-25 13:30:01 -0500

    Create The basics.py

---
+++ The basics.py
@@ -0,0 +1,64 @@
+#Shravya Aspari
+#25/02/25
+
+
+#Different types of variables
+
+#Integers
+print("Integers are whole number that are positive or negative.")
+x=4
+print("For example:",x) 
+
+
+#Floats
+print("Floats are number that inlude decimal points.")
+y=4.528
+print("For example:",y)
+
+#Char
+print("Chars are single letter characters.")
+c="c"
+print("For example:",c)
+
+#Booleans
+print("Booleans are True or False statements.")
+print("For example:")
+Yes=True
+No=False
+print(Yes)
+print(No)
+
+#Strings
+print("Strings are multiple letter characters. For example, all the words you see on the screen are strings")
+
+
+#Calculating with different math operations
+import math
+
+#Addition
+sum_add=5+9
+print("5 plus 9 is equal to",sum_add)
+
+#Subtraction
+difference=9-5
+print("9 minus 5 is equal to",difference)
+
+#Multiplication
+product=5*9
+print("5 multiplied by 9 is equal to",product)
+
+#Division
+quotient=45/5
+print("45 divided by 5 is equal to",quotient)
+
+#Exponents
+exponent=5**3
+print("5 to the power of 3 is equal to",exponent)
+
+#Square Root
+root=math.sqrt(81)
+print("The square root of 81 is equal to",(int(root)))
+
+#Modulus(the modulus is the remainder of a division question)
+remainder=125%9
+print("The remainder of 125 divided by 9 is equal to",remainder)
